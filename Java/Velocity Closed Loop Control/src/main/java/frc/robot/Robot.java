d85bcdb499bd46e8eb18cee7ca7a566df15112f2
lesser618 <lesser618@gmail.com>
2020-02-15 09:53:17 -0700

    added second motor

--- Java/Velocity Closed Loop Control/src/main/java/frc/robot/Robot.java
+++ Java/Velocity Closed Loop Control/src/main/java/frc/robot/Robot.java
@@ -24,52 +24,69 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class Robot extends TimedRobot {
   private Joystick m_stick;
   private static final int deviceID = 1;
-  private CANSparkMax m_motor;
-  private CANPIDController m_pidController;
-  private CANEncoder m_encoder;
-  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+  private CANSparkMax m_Motor1;
+  private CANSparkMax m_Motor2;
+  private CANPIDController m_pidController1;
+  private CANPIDController m_pidController2;
+  private CANEncoder m_encoder1;
+  private CANEncoder m_encoder2;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, RPM;
 
   @Override
   public void robotInit() {
     m_stick = new Joystick(0);
 
     // initialize motor
-    m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
+    m_Motor1 = new CANSparkMax(1, MotorType.kBrushless);
+    m_Motor2 = new CANSparkMax(2, MotorType.kBrushless);
+
+    RPM = 5676;
+
 
     /**
      * The RestoreFactoryDefaults method can be used to reset the configuration parameters
      * in the SPARK MAX to their factory default state. If no argument is passed, these
      * parameters will not persist between power cycles
      */
-    m_motor.restoreFactoryDefaults();
+    m_Motor1.restoreFactoryDefaults();
+    m_Motor2.restoreFactoryDefaults();
 
     /**
      * In order to use PID functionality for a controller, a CANPIDController object
      * is constructed by calling the getPIDController() method on an existing
      * CANSparkMax object
      */
-    m_pidController = m_motor.getPIDController();
+    m_pidController1 = m_Motor1.getPIDController();
+    m_pidController2 = m_Motor2.getPIDController();
 
     // Encoder object created to display position values
-    m_encoder = m_motor.getEncoder();
+    m_encoder1 = m_Motor1.getEncoder();
+    m_encoder2 = m_Motor2.getEncoder();
 
     // PID coefficients
-    kP = 6e-5; 
+    kP = 6e-4;
     kI = 0;
-    kD = 0; 
-    kIz = 0; 
-    kFF = 0.000015; 
-    kMaxOutput = 1; 
-    kMinOutput = -1;
+    kD = 0;
+    kIz = 0;
+    kFF = 0.000015;
+    kMaxOutput = 5700;
+    kMinOutput = -5700;
     maxRPM = 5700;
 
     // set PID coefficients
-    m_pidController.setP(kP);
-    m_pidController.setI(kI);
-    m_pidController.setD(kD);
-    m_pidController.setIZone(kIz);
-    m_pidController.setFF(kFF);
-    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+    m_pidController1.setP(kP);
+    m_pidController1.setI(kI);
+    m_pidController1.setD(kD);
+    m_pidController1.setIZone(kIz);
+    m_pidController1.setFF(kFF);
+    m_pidController1.setOutputRange(kMinOutput, kMaxOutput);
+
+    m_pidController2.setP(kP);
+    m_pidController2.setI(kI);
+    m_pidController2.setD(kD);
+    m_pidController2.setIZone(kIz);
+    m_pidController2.setFF(kFF);
+    m_pidController2.setOutputRange(kMinOutput, kMaxOutput);
 
     // display PID coefficients on SmartDashboard
     SmartDashboard.putNumber("P Gain", kP);
@@ -79,6 +96,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Feed Forward", kFF);
     SmartDashboard.putNumber("Max Output", kMaxOutput);
     SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("RPM demand", RPM);
   }
 
   @Override
@@ -91,36 +109,51 @@ public class Robot extends TimedRobot {
     double ff = SmartDashboard.getNumber("Feed Forward", 0);
     double max = SmartDashboard.getNumber("Max Output", 0);
     double min = SmartDashboard.getNumber("Min Output", 0);
+    double RPM = SmartDashboard.getNumber("RPM", 0);
+    
 
     // if PID coefficients on SmartDashboard have changed, write new values to controller
-    if((p != kP)) { m_pidController.setP(p); kP = p; }
-    if((i != kI)) { m_pidController.setI(i); kI = i; }
-    if((d != kD)) { m_pidController.setD(d); kD = d; }
-    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
-    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
-    if((max != kMaxOutput) || (min != kMinOutput)) { 
-      m_pidController.setOutputRange(min, max); 
-      kMinOutput = min; kMaxOutput = max; 
+    if((p != kP)) { m_pidController1.setP(p); kP = p; }
+    if((i != kI)) { m_pidController1.setI(i); kI = i; }
+    if((d != kD)) { m_pidController1.setD(d); kD = d; }
+    if((iz != kIz)) { m_pidController1.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController1.setFF(ff); kFF = ff; }
+    if((RPM != RPM)) { RPM = RPM; }
+    if((max != kMaxOutput) || (min != kMinOutput)) {
+      m_pidController1.setOutputRange(min, max);
+      kMinOutput = min; kMaxOutput = max;
+    }
+
+    if((p != kP)) { m_pidController2.setP(p); kP = p; }
+    if((i != kI)) { m_pidController2.setI(i); kI = i; }
+    if((d != kD)) { m_pidController2.setD(d); kD = d; }
+    if((iz != kIz)) { m_pidController2.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController2.setFF(ff); kFF = ff; }
+    if((RPM != RPM)) { RPM = RPM; }
+    if((max != kMaxOutput) || (min != kMinOutput)) {
+      m_pidController2.setOutputRange(min, max);
+      kMinOutput = min; kMaxOutput = max;
     }
 
     /**
-     * PIDController objects are commanded to a set point using the 
+     * PIDController objects are commanded to a set point using the
      * SetReference() method.
-     * 
+     *
      * The first parameter is the value of the set point, whose units vary
      * depending on the control type set in the second parameter.
-     * 
-     * The second parameter is the control type can be set to one of four 
+     *
+     * The second parameter is the control type can be set to one of four
      * parameters:
      *  com.revrobotics.ControlType.kDutyCycle
      *  com.revrobotics.ControlType.kPosition
      *  com.revrobotics.ControlType.kVelocity
      *  com.revrobotics.ControlType.kVoltage
      */
-    double setPoint = m_stick.getY()*maxRPM;
-    m_pidController.setReference(setPoint, ControlType.kVelocity);
-    
-    SmartDashboard.putNumber("SetPoint", setPoint);
-    SmartDashboard.putNumber("ProcessVariable", m_encoder.getVelocity());
+
+    m_pidController1.setReference(-1140, ControlType.kVelocity);
+    m_pidController2.setReference(1140, ControlType.kVelocity);
+
+    SmartDashboard.putNumber("ProcessVariable", m_encoder1.getVelocity());
+    SmartDashboard.putNumber("ProcessVariable", m_encoder2.getVelocity());
   }
 }
